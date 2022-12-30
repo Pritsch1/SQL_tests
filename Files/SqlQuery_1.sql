0649e374b495e93b13e5dd9dbea33ed45d93680c
Pritsch <pritschdec@gmail.com>
2022-12-30 00:58:58 -0300

    SQL is SQL
    o/

---
+++ Files/SqlQuery_1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE student(
+student_id INT, 
+names VARCHAR(30),
+major VARCHAR(20),
+PRIMARY KEY(student_id)
+);
+
+select * from student;
+
+insert student(student_id, names, major) values(1, 'Bruno Pritsch Dec', 'Computer Science');
+insert student(student_id, names, major) values(2, 'Luiza Pritsch Dec', 'Environmental Eng.');
+insert student(student_id, names, major) values(3, 'Adriano Alcantara de Lima', 'Acaikfa...');
+insert student(student_id, names, major) values(4, 'Guilherme Rodrigues', 'Logistics');
+
+select * from student;
+UPDATE student
+SET major = 'Accounting'
+WHERE major = 'Acaikfa...';
